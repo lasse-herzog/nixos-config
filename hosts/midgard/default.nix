18c827308a2a621508193aab3679cd0b70e3444d
Lasse Herzog <lasse@herzoeglich.de>
2025-08-21 12:12:38 +0200

    Add hardware-configuration.nix

--- hosts/midgard/default.nix
+++ hosts/midgard/default.nix
@@ -3,40 +3,41 @@
   inputs,
   specialArgs,
   mylib,
+  lib,
   ...
 }: {
-  imports =
+  imports = lib.flatten [
+    ./hardware-configuration.nix
+    inputs.agenix.nixosModules.default
+
+    inputs.nixos-hardware.nixosModules.common-cpu-amd-pstate
+    inputs.nixos-hardware.nixosModules.common-pc-ssd
+
+    inputs.catppuccin.nixosModules.catppuccin
+    inputs.musnix.nixosModules.musnix
+
+    inputs.home-manager.nixosModules.home-manager
+    {
+      home-manager = {
+        backupFileExtension = "backup";
+        extraSpecialArgs = specialArgs;
+
+        useGlobalPkgs = true;
+        useUserPackages = true;
+
+        users.admin.imports = [
+          ./home/home.nix
+          inputs.catppuccin.homeModules.catppuccin
+          inputs.nvchad4nix.homeManagerModule
+          inputs.spicetify-nix.homeManagerModules.default
+          inputs.zen-browser.homeModules.twilight
+        ];
+      };
+    }
+
     (map mylib.relativeToRoot [
       "configuration.nix"
       "secrets"
     ])
-    ++ [
-      ./hardware-configuration.nix
-      inputs.agenix.nixosModules.default
-
-      inputs.nixos-hardware.nixosModules.common-cpu-amd-pstate
-      inputs.nixos-hardware.nixosModules.common-pc-ssd
-
-      inputs.catppuccin.nixosModules.catppuccin
-      inputs.musnix.nixosModules.musnix
-
-      inputs.home-manager.nixosModules.home-manager
-      {
-        home-manager = {
-          backupFileExtension = "backup";
-          extraSpecialArgs = specialArgs;
-
-          useGlobalPkgs = true;
-          useUserPackages = true;
-
-          users.admin.imports = [
-            ./home/home.nix
-            inputs.catppuccin.homeModules.catppuccin
-            inputs.nvchad4nix.homeManagerModule
-            inputs.spicetify-nix.homeManagerModules.default
-            inputs.zen-browser.homeModules.twilight
-          ];
-        };
-      }
-    ];
+  ];
 }
